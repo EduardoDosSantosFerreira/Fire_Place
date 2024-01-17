02b40dc27df9127e8840b7f954c16684273ce947
EduardoDosSantosFerreira <eduardosferreira69@gmail.com>
2024-01-17 18:04:47 -0300

    lib_update_v6

--- src/data/assets/js/scripts/main.js
+++ src/data/assets/js/scripts/main.js
@@ -1,8 +1,8 @@
-// Nav
 document.addEventListener("DOMContentLoaded", function () {
   var openMenuButton = document.getElementById("map");
   var closeMenuButton = document.getElementById("close-menu");
   var offCanvasMenu = document.querySelector(".off-canvas-menu");
+  var viagensDropdown = document.getElementById("viagens-dropdown");
 
   openMenuButton.addEventListener("click", function () {
     offCanvasMenu.style.right = "0"; // Abre o menu deslizando do lado direito
@@ -13,6 +13,19 @@ document.addEventListener("DOMContentLoaded", function () {
     offCanvasMenu.style.right = "-300px"; // Fecha o menu deslizando para fora do lado direito
     openMenuButton.style.display = "block";
   });
-});
 
-// /Nav
+  // Adicionando funcionalidade ao dropdown "Viagens"
+  viagensDropdown.addEventListener("click", function (event) {
+    var dropdownContent = this.querySelector(".dropdown-content");
+    dropdownContent.classList.toggle("show");
+    event.stopPropagation(); // Impede a propagação do evento para o window click handler
+  });
+
+  // Fechar dropdown se clicar fora dele
+  window.addEventListener("click", function (event) {
+    if (event.target !== viagensDropdown && !viagensDropdown.contains(event.target)) {
+      var dropdownContent = viagensDropdown.querySelector(".dropdown-content");
+      dropdownContent.classList.remove("show");
+    }
+  });
+});
