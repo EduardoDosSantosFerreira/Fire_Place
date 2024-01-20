8cf72ac9ba047d419f3547851ad7edf2bbe028af
EduardoDosSantosFerreira <eduardosferreira69@gmail.com>
2024-01-19 23:31:32 -0300

    bag_update_alpha_v1

--- src/data/assets/js/npc/dialogue01.js
+++ src/data/assets/js/npc/dialogue01.js
@@ -1,29 +1,59 @@
-var dialogos = [
-  "Oh, olá, adorável viajante ! Que alegria ter você em minha humilde morada! Bem-vindo ao meu cantinho aconchegante. Espero que sua jornada tenha sido segura e repleta descobertas!",
-  "Ah, vejo que você é uma alma curiosa, sempre em busca de novas experiências. Que tal fazermos trocas de histórias e conhecimento? Aqui, no meu lar, compartilhamos uns com os outros como uma grande família. ",
-  "Vá para a biblioteca, minha amada Serana ira lhe recepcionar. ",
-
+const dialogos = [
+  "Oh, olá, adorável viajante! Que alegria ter você em minha humilde morada! Bem-vindo ao meu cantinho aconchegante. Espero que sua jornada tenha sido segura e repleta de descobertas!",
+  "Ah, vejo que você é uma alma curiosa, sempre em busca de novas experiências. Que tal fazermos trocas de histórias e conhecimento? Aqui, no meu lar, compartilhamos uns com os outros como uma grande família.",
+  "Vá para a biblioteca, minha amada Serana irá lhe recepcionar."
 ];
 
-var indiceDialogoAtual = 0;
+let indiceDialogoAtual = 0;
 
-function proximoDialogo() {
+const proximoDialogo = () => {
   if (indiceDialogoAtual < dialogos.length - 1) {
     indiceDialogoAtual++;
     exibirDialogoAtual();
   }
-}
+};
 
-function dialogoAnterior() {
+const dialogoAnterior = () => {
   if (indiceDialogoAtual > 0) {
     indiceDialogoAtual--;
     exibirDialogoAtual();
   }
-}
+};
 
-function exibirDialogoAtual() {
-  document.getElementById("Eldrystandialogue").innerHTML =
-    "<p>" + dialogos[indiceDialogoAtual] + "</p>";
-}
+const exibirDialogoAtual = () => {
+  const eldrystanDialogue = document.getElementById("Eldrystandialogue");
+  eldrystanDialogue.innerHTML = `<p>${dialogos[indiceDialogoAtual]}</p>`;
+};
+
+document.querySelector('.dialogue-container').classList.add('show');
+
+// Serana's Dialogue
+
+const seranaDialogos = [
+  "Oh, você é um novo rosto por aqui! Que alegria ter um visitante tão encantador. Poderia me contar seu nome?",
+  "Bem-vindo(a), [Nome]. É um prazer conhecê-lo(a). Espero que tenha tido uma jornada agradável até aqui.",
+  "Sinta-se à vontade para explorar nosso refúgio e conversar. Estou sempre interessada em conhecer novas histórias e experiências."
+];
+
+let indiceDialogoSeranaAtual = 0;
+
+const proximoDialogoSerana = () => {
+  if (indiceDialogoSeranaAtual < seranaDialogos.length - 1) {
+    indiceDialogoSeranaAtual++;
+    exibirDialogoSeranaAtual();
+  }
+};
+
+const dialogoAnteriorSerana = () => {
+  if (indiceDialogoSeranaAtual > 0) {
+    indiceDialogoSeranaAtual--;
+    exibirDialogoSeranaAtual();
+  }
+};
+
+const exibirDialogoSeranaAtual = () => {
+  const seranaDialogue = document.getElementById("Seranadialogue");
+  seranaDialogue.innerHTML = `<p>${seranaDialogos[indiceDialogoSeranaAtual]}</p>`;
+};
 
 document.querySelector('.dialogue-container').classList.add('show');
--- src/data/assets/js/scripts/logos.js
+++ src/data/assets/js/scripts/logos.js
@@ -1,7 +1,25 @@
-  document.addEventListener('DOMContentLoaded', function() {
+// FirePlace
+document.addEventListener('DOMContentLoaded', function() {
     var fireText = document.getElementById('fire-text');
     
     fireText.addEventListener('animationend', function() {
       fireText.style.display = 'none'; // Remover o elemento do DOM após a animação
     });
   });
+
+  // Shop
+  // Função para exibir o texto e iniciar o efeito de desaparecimento
+  function mostrarTextoTemporario() {
+    var elementoTexto = document.getElementById("shop-text");
+
+    // Mostrar o texto
+    elementoTexto.style.opacity = 1;
+
+    // Iniciar o efeito de desaparecimento
+    setTimeout(function() {
+      elementoTexto.style.opacity = 0;
+    }, 2000); // 2000 milissegundos = 2 segundos (ajuste conforme necessário)
+  }
+
+  // Chamar a função quando a página carregar
+  window.onload = mostrarTextoTemporario;
